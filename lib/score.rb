51f082528c90bb7df3eb9f9098dec83ce15b1cde
ChrisWcs <chansen@mavenlink.com>
2020-03-07 22:14:25 -0700

    Calculates bonus for all

--- lib/score.rb
+++ lib/score.rb
@@ -14,18 +14,24 @@ class Score
     self
   end
 
-  def with_cheese(num_of_cheese)
+  def with_cheese(num_of_cheese, bonus)
     @score += num_of_cheese * 3
+    @score += 15 if bonus == :king
+    @score += 10 if bonus == :queen
     self
   end
 
-  def with_bread(num_of_bread)
+  def with_bread(num_of_bread, bonus)
     @score += num_of_bread * 3
+    @score += 15 if bonus == :king
+    @score += 10 if bonus == :queen
     self
   end
 
-  def with_chicken(num_of_chicken)
+  def with_chicken(num_of_chicken, bonus)
     @score += num_of_chicken * 4
+    @score += 10 if bonus == :king
+    @score += 5 if bonus == :queen
     self
   end
 end
--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -31,24 +31,60 @@ RSpec.describe Score, "#calculate" do
   context "with_cheese" do
     it "calculates points based on the cheese given" do
       score = Score.new
-      score.with_cheese(1)
+      score.with_cheese(1, :none)
       expect(score.calculate).to eq 3
+    end
+
+    it "calculates points based on the cheese given and a king bonus" do
+      score = Score.new
+      score.with_cheese(1, :king)
+      expect(score.calculate).to eq 18
+    end
+
+    it "calculates points based on the cheese given and a queen bonus" do
+      score = Score.new
+      score.with_cheese(1, :queen)
+      expect(score.calculate).to eq 13
     end
   end
 
   context "with_bread" do
     it "calculates points based on the bread given" do
       score = Score.new
-      score.with_bread(1)
+      score.with_bread(1, :none)
       expect(score.calculate).to eq 3
+    end
+
+    it "calculates points based on the bread given and a king bonus" do
+      score = Score.new
+      score.with_bread(1, :king)
+      expect(score.calculate).to eq 18
+    end
+
+    it "calculates points based on the bread given and a queen bonus" do
+      score = Score.new
+      score.with_bread(1, :queen)
+      expect(score.calculate).to eq 13
     end
   end
 
   context "with_chicken" do
     it "calculates points based on the chicken given" do
       score = Score.new
-      score.with_chicken(1)
+      score.with_chicken(1, :none)
       expect(score.calculate).to eq 4
+    end
+
+    it "calculates points based on the chicken given and a king bonus" do
+      score = Score.new
+      score.with_chicken(1, :king)
+      expect(score.calculate).to eq 14
+    end
+
+    it "calculates points based on the chicken given and a queen bonus" do
+      score = Score.new
+      score.with_chicken(1, :queen)
+      expect(score.calculate).to eq 9
     end
   end
 
@@ -56,9 +92,9 @@ RSpec.describe Score, "#calculate" do
     it "calculates the score given both" do
       score = Score.new
 
-      score.with_cheese(1)
+      score.with_cheese(1, :none)
            .with_apples(1, :none)
-           .with_bread(1)
+           .with_bread(1, :none)
 
       expect(score.calculate).to eq 8
     end
