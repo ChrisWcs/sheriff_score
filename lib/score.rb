64838e0cc03e17f6aa5486106063a853716e7f19
ChrisWcs <chansen@mavenlink.com>
2020-03-07 22:57:24 -0700

    refactors

--- lib/score.rb
+++ lib/score.rb
@@ -1,3 +1,65 @@
+class Goods
+  def initialize(king, queen)
+    @king = king
+    @queen = queen
+  end
+
+  def calc_bonus(bonus)
+    case bonus
+    when :king
+      @king
+    when :queen
+      @queen
+    when :none
+      0
+    end
+  end
+end
+
+class Apples < Goods
+  def initialize
+    super(20, 10)
+  end
+  def calculate(bonus, num_of_apples)
+    score = num_of_apples * 2
+    score += calc_bonus(bonus)
+    score
+  end
+end
+
+class Cheese < Goods
+  def initialize
+    super(15, 10)
+  end
+  def calculate(bonus, num_of_cheeses)
+    score = num_of_cheeses * 3
+    score += calc_bonus(bonus)
+    score
+  end
+end
+
+class Bread < Goods
+  def initialize
+    super(15, 10)
+  end
+  def calculate(bonus, num_of_bread)
+    score = num_of_bread * 3
+    score += calc_bonus(bonus)
+    score
+  end
+end
+
+class Chicken < Goods
+  def initialize
+    super(10, 5)
+  end
+  def calculate(bonus, num_of_chicken)
+    score = num_of_chicken * 4
+    score += calc_bonus(bonus)
+    score
+  end
+end
+
 class Score
   def initialize
     @score = 0
@@ -13,30 +75,22 @@ class Score
   end
 
   def with_apples(num_of_apples, bonus)
-    @score += num_of_apples * 2
-    @score += 20 if bonus == :king
-    @score += 10 if bonus == :queen
+    @score += Apples.new.calculate(bonus, num_of_apples)
     self
   end
 
   def with_cheese(num_of_cheese, bonus)
-    @score += num_of_cheese * 3
-    @score += 15 if bonus == :king
-    @score += 10 if bonus == :queen
+    @score += Cheese.new.calculate(bonus, num_of_cheese)
     self
   end
 
   def with_bread(num_of_bread, bonus)
-    @score += num_of_bread * 3
-    @score += 15 if bonus == :king
-    @score += 10 if bonus == :queen
+    @score += Bread.new.calculate(bonus, num_of_bread)
     self
   end
 
   def with_chicken(num_of_chicken, bonus)
-    @score += num_of_chicken * 4
-    @score += 10 if bonus == :king
-    @score += 5 if bonus == :queen
+    @score += Chicken.new.calculate(bonus, num_of_chicken)
     self
   end
 end
