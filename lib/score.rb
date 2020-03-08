ad13487a6ab9c4e29c877ce32d76307d80e541d5
ChrisWcs <chansen@mavenlink.com>
2020-03-07 21:07:14 -0700

    Adds up goods

--- lib/score.rb
+++ lib/score.rb
@@ -1,6 +1,29 @@
 class Score
+  def initialize
+    @score = 0
+  end
 
   def calculate
-    0
+    @score
+  end
+
+  def with_apples(num_of_apples)
+    @score += num_of_apples * 2
+    self
+  end
+
+  def with_cheese(num_of_cheese)
+    @score += num_of_cheese * 3
+    self
+  end
+
+  def with_bread(num_of_bread)
+    @score += num_of_bread * 3
+    self
+  end
+
+  def with_chicken(num_of_chicken)
+    @score += num_of_chicken * 4
+    self
   end
 end
--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -7,4 +7,48 @@ RSpec.describe Score, "#calculate" do
       expect(score.calculate).to eq 0
     end
   end
+
+  context "with_apples" do
+    it "calculates points based on the apples given" do
+      score = Score.new
+      score.with_apples(1)
+      expect(score.calculate).to eq 2
+    end
+  end
+
+  context "with_cheese" do
+    it "calculates points based on the cheese given" do
+      score = Score.new
+      score.with_cheese(1)
+      expect(score.calculate).to eq 3
+    end
+  end
+
+  context "with_bread" do
+    it "calculates points based on the bread given" do
+      score = Score.new
+      score.with_bread(1)
+      expect(score.calculate).to eq 3
+    end
+  end
+
+  context "with_chicken" do
+    it "calculates points based on the chicken given" do
+      score = Score.new
+      score.with_chicken(1)
+      expect(score.calculate).to eq 4
+    end
+  end
+
+  context "with both cheese, apples, and bread" do
+    it "calculates the score given both" do
+      score = Score.new
+
+      score.with_cheese(1)
+           .with_apples(1)
+           .with_bread(1)
+
+      expect(score.calculate).to eq 8
+    end
+  end
 end
